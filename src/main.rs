ac9bd44b6b3533305692af4a22a76d5a9908a4a7
dymocaptin <peknudsen@gmail.com>
2018-08-13 23:14:10 -0700

    remove spaces

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::env;
 
 fn main() {
 	use std::path::Path;
-
+    
     for arg in env::args_os().skip(1) {
         println!("opening port: {:?}", arg);
         let mut port = serial::unix::TTYPort::open(Path::new(&arg)).unwrap();
