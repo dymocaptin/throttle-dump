c3fea5f03e263e3f85921e24c9cb7f9d860e2880
dymocaptin <peknudsen@gmail.com>
2018-08-13 22:55:26 -0700

    super serial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+extern crate serial;
+
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+	use std::path::Path;
+
+    for arg in env::args_os().skip(1) {
+        println!("opening port: {:?}", arg);
+        let mut port = serial::unix::TTYPort::open(Path::new(&arg)).unwrap();
+    }
 }
